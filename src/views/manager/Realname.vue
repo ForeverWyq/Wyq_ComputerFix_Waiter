47a3eeaa5eab84d694a3418cb218d56ce86b142d
ForeverWyq <987043378@qq.com>
2020-10-21 12:21:04 +0800

    新增实名认证

--- src/views/manager/Realname.vue
+++ src/views/manager/Realname.vue
@@ -2,8 +2,18 @@
     <div class="realname">
         <wyq-fulllayout title="实名认证">
             <div class="header">
-                <van-cell title="请上传身份证正反面照" size="large" />
-                <van-uploader class="uploader" v-model="fileList" max-count="2" :after-read="afterRead" />
+                <van-cell title="请上传身份证正面照" size="large" />
+                <van-uploader :after-read="afterRead1" >
+                    <div class="photo">
+                        <img :src="photo1" alt=""/>
+                    </div>
+                </van-uploader>
+                <van-cell title="请上传身份证反面照" size="large" />
+                <van-uploader :after-read="afterRead2" >
+                    <div class="photo">
+                        <img :src="photo2" alt=""/>
+                    </div>
+                </van-uploader>
                 <!-- 验证 -->
                 <div class="btn" @click="RealnameHandler">
                     验证
@@ -25,47 +35,76 @@ Vue.use(Toast);
 export default {
     data() {
         return {
-            fileList: [],
-            photo:[],
-            formData:[]
+            photo1: '',
+            photo2: '',
         }
     },
     computed: {
-        ...mapState('user',['info']),
-        ...mapState('realname',['waiterrealname'])
+        ...mapState('user',['info', 'waiter']),
     },
     created() {
-
+        this.loadidimg();
     },
     methods: {
-        ...mapActions('realname',['WaiterRealname']),
+        ...mapActions('user',['FindWaiterById', 'UpdateWaiter']),
+        loadidimg(){
+            this.FindWaiterById(this.info.id).then(()=>{
+                this.photo1 = this.waiter.idcardPhotoPositive
+                this.photo2 = this.waiter.idcardPhotoNegative
+            })
+        },
+        // 身份证正面
+        afterRead1(file) {
+            this.uploadImg(file.file,1)
+        },
+        // 身份证背面
+        afterRead2(file) {
+            this.uploadImg(file.file,2)
+        },
+        // 上传图片到服务器
+        uploadImg (file,whichphoto) {
+            // 创建form对象
+            let formdata = new FormData();
+            // 通过append向form对象添加数据,可以通过append继续添加数据
+            //或formdata.append('file',file);
+            formdata.append('file', file, file.name);
+            //设置请求头
+            let config = {
+                headers:{
+                    'Content-Type':'multipart/form-data'
+                }
+            };  
+            //this.axios 是因为在main.js写在vue实例里
+            const axiosAjax = axios.create({
+                timeout: 1000 * 60, //时间
+                withCredentials: true //跨域携带cookie
+            });
+            axiosAjax.post('http://121.199.29.84:8001/file/upload',formdata,config)
+            .then((res)=>{   //这里的url为后端接口
+                //res 为接口返回值
+                if(whichphoto==1){
+                    this.photo1 = "http://121.199.29.84:8888/group1/"+res.data.data.id
+                    console.log(this.photo1);
+                }else if(whichphoto==2){
+                    this.photo2 = "http://121.199.29.84:8888/group1/"+res.data.data.id
+                    console.log(this.photo2);
+                }
+            }).catch((response) => {})
+        },
         // 实名验证
         RealnameHandler(){
-            this.params = {
-                user_id:this.info.id
-            }
-            axios.post('http://134.175.100.63:5588/user/realname?user_id='+this.info.id, this.formData)
-            .then(res => { 
-                Toast.success(res.data);　　　　
-        　　});
-            // 此时不需要再调用一次接口
-            // this.WaiterRealname(this.params)
-        },
-        afterRead(file) {
-            //构造一个 FormData，把后台需要发送的参数添加
-            this.formData = new FormData(); 
-            //接口需要传的参数
-            this.photo.push(file)
-            this.photo.map((item,index)=>{
-                if(index == 0){
-                    this.formData.append('file1',item.file)
-                }
-                if(index == 1){
-                    this.formData.append('file2',item.file)
-                }
+            console.log(this.photo1);
+            console.log(this.photo2);
+            let params = this.waiter;
+            params.idcardPhotoPositive = this.photo1;
+            params.idcardPhotoNegative = this.photo2;
+            this.UpdateWaiter(params)
+            .then(res=>{
+                // console.log(this.info.id)
+                this.FindWaiterById(this.info.id).then(()=>{
+                    this.$toast('上传成功');
+                })
             })
-            console.log(this.formData.getAll('file1'),'file1')
-            console.log(this.formData.getAll('file2'),'file2')
         },
     }
 }
@@ -73,19 +112,35 @@ export default {
 
 <style scoped>
 .header{
+    text-align: center;
     margin-top: 50px ;
 }
-.uploader{
-    margin-left: 20px;
-}
 .btn {
-  background-color: #ededed;
-  width: 90%;
-  margin: 2em auto;
-  line-height: 3em;
-  text-align: center;
-  border: 1px solid #ededed;
-  border-radius: 1.5em;
-  font-size: 14px;
+    background-color: #ededed;
+    width: 90%;
+    margin: 2em auto;
+    line-height: 3em;
+    text-align: center;
+    border: 1px solid #ededed;
+    border-radius: 1.5em;
+    font-size: 14px;
+}
+.photo {
+    margin: 0 auto;
+    width: 15em;
+    height: 10em;
+    box-sizing: border-box;
+    /* border:1px solid #1659a0; */
+    overflow:hidden;
+    padding: 1em;
+}
+.header .photo img {
+    background: #fff;
+    background-image: url(../../assets/isnull.png);
+    background-repeat: no-repeat;
+    background-position: center;
+    text-align: center;
+    width: 100%;
+    height: 100%;
 }
 </style>
